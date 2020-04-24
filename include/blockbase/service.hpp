c341aa61a77a3c9ef28f6b1d559b5fa293c28ec3
dbulha <49444700+dbulha@users.noreply.github.com>
2020-04-24 11:47:15 +0100

    Remove punish to warning type 1

--- include/blockbase/service.hpp
+++ include/blockbase/service.hpp
@@ -3,16 +3,6 @@ static std::map<eosio::name, asset> GetProducersToPunishInfo(const eosio::name& 
     blockbase::blacklistIndex _blacklists(contract, owner.value);
     std::map<eosio::name, asset> producersToPunish;
     eosio::asset stake;
-    if(std::distance(_producers.begin(), _producers.end()) > 0) {
-        for(auto producer : _producers){
-            if(producer.warning_type == 1) {	
-                stake = blockbasetoken::get_stake(BLOCKBASE_TOKEN, owner, producer.key);
-                uint32_t updatedStake = (stake.amount*0.3);
-                stake = asset(updatedStake, symbol(eosio::symbol_code("BBT"), 4));
-                producersToPunish.insert(std::pair<eosio::name,asset>(producer.key,stake));
-            }
-        }
-    }
     if(std::distance(_blacklists.begin(), _blacklists.end()) > 0){
         for(auto blackListedProducer : _blacklists) {
             stake = asset(1, symbol(eosio::symbol_code("BBT"), 4));
